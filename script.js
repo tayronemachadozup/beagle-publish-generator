07633d75a88cecf1e82bb037bfe1dcbbdfeba1da
tayronearaujo <tayron_machado@hotmail.com>
2021-01-29 10:20:38 -0300

    added package.json

--- script.js
+++ script.js
@@ -13,35 +13,37 @@ impactosArray = impactos.split('|');
 console.log(impactosArray)
 var plataforma = localStorage.getItem("plataforma");
 
-var release = "<div>"+ 
-"<img src='./assets/Release.svg'>"+
-"<div>"+
-"<a>Versão"+versao+"</a>"+
-"</div>"+
-"</div>"
+var release = "<div>" +
+  "<img src='./assets/Release.svg'>" +
+  "<div>" +
+  "<a>Versão" + versao + "</a>" +
+  "</div>" +
+  "</div>"
 
-var novasFuncionalidades = "<div>"+ 
-"<ul>"+
-funcionalidadesArray.map(function(item){
-  return "<li>" + item + "</li>" 
-}).join('');
-+"</ul>"+
-"</div>"
+var novasFuncionalidades = "<div>" +
+  "<ul>" +
+  funcionalidadesArray.map(function (item) {
+    return "<li>" + item + "</li>"
+  }).join('');
++"</ul>" +
+  "</div>"
 
-var Novosimpactos = "<div>"+ 
-"<ul>"+
-impactosArray.map(function(item){
-  return "<li>" + item + "</li>"
-}).join('');
-+"</ul>"+
-"</div>"
+var Novosimpactos = "<div>" +
+  "<ul>" +
+  impactosArray.map(function (item) {
+    return "<li>" + item + "</li>"
+  }).join('');
++"</ul>" +
+  "</div>"
 
 
 var Novaplataforma = "<a>" +
- plataforma + 
- "</a>"
+  plataforma +
+  "</a>"
 
-fileList = './publish/template-web.html'; 
-fs.readFile(fileList, function(err, data) { if(err) throw err; data = data.toString(); data = data.replace('<div id="Release"></div>', release), data = data.replace('<div id="Funcionalidades"></div>', novasFuncionalidades), data = data.replace('<div id="Impactos"></div>', Novosimpactos), data = data.replace('<a id="Plataforma"></a>', Novaplataforma)
-console.log("replace", data)
-fs.writeFile('./complete/index.html', data, function(err) { err || console.log('Data replaced \n', data); })});
+fileList = './publish/template-web.html';
+fs.readFile(fileList, function (err, data) {
+  if (err) throw err; data = data.toString(); data = data.replace('<div id="Release"></div>', release), data = data.replace('<div id="Funcionalidades"></div>', novasFuncionalidades), data = data.replace('<div id="Impactos"></div>', Novosimpactos), data = data.replace('<a id="Plataforma"></a>', Novaplataforma)
+  console.log("replace", data)
+  fs.writeFile('./complete/index.html', data, function (err) { err || console.log('Data replaced \n', data); })
+});
